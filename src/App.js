4b668e5bf9c6580fec8a5b9d7753497ce0552a73
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-08 08:01:39 -0700

    add some files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 // src/App.js
+import Calculator from './Components/calculator';
 import React from 'react';
 
 function App() {
-  return <div>Hello React</div>;
+  return <div>Hello React
+    <Calculator />
+  </div>;
 }
 
 export default App;
--- src/Components/calculator.js
+++ src/Components/calculator.js
@@ -1,11 +1,47 @@
+// Calculator.js
 import React from "react";
+import "./calculator.css"; // Import the CSS file
 
-const calculator = () => {
-   return(
-    <div>
-        Hello calculator
-    </div>
-   )
+function Button({ children, isOperation, isDouble }) {
+  const buttonClassName = isOperation ? "operation-button" : "";
+  const buttonStyle = isDouble ? { gridColumn: "span 2" } : {};
+  return (
+    <button className={buttonClassName} style={buttonStyle}>
+      {children}
+    </button>
+  );
 }
 
-export default calculator;
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="display">0</div>
+      <div className="button-row">
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button>
+        <Button isOperation>+</Button>
+      </div>
+      <div className="button-row">
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
+        <Button isOperation>-</Button>
+      </div>
+      <div className="button-row">
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button isOperation>*</Button>
+      </div>
+      <div className="button-row">
+        <Button isDouble>0</Button>
+        <Button>C</Button>
+        <Button>=</Button>
+        <Button isOperation>/</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
