e0daa217017971cf9878b03b241b7f79b43c3573
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-09 05:50:24 -0700

    create the calculator structure

--- src/Components/calculator.js
+++ src/Components/calculator.js
@@ -1,34 +1,34 @@
-import './calculator.css'
+import "./calculator.css";
 function Calculator() {
-  return(
-  <div className='calculator'>
-  <div className=''>
-
-  </div>
-  <div className="calculator-grid">
-    <button>AC</button>
-    <button>+/-</button>
-    <button>%</button>
-    <button>/</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button>*</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button>-</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>+</button>
-    <button className='span-two'>0</button>
-     <button>.</button>
-      <button>=</button>
-  
-  </div>
-  </div>
-  ) 
+  return (
+    <div className="calculator">
+       
+          <input className="calculator-screen" type="text" class="calculator-screen" value="0" disabled />
+    
+      <div className="calculator-grid">
+       
+        <button>AC</button>
+        <button>+/-</button>
+        <button>%</button>
+        <button className="oper">/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="oper">*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button className="oper">-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className="oper">+</button>
+        <button className="span-two">0</button>
+        <button>.</button>
+        <button className="oper">=</button>
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
