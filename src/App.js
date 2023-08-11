ddcfaa44014110877842ea31eb73fbee7989251c
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-11 05:35:55 -0700

    ADd fetch request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // src/App.js
 import React from 'react';
 import Calculator from './Components/calculator';
+import Quotes from './Components/FetchAPI';
 
 function App() {
   return (
     <div>
-    
+      <Quotes />
       <Calculator />
     </div>
   );
